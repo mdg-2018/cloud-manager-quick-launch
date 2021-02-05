ced11f12c3204dadde6a0b8d1cdbcec8841ee4b8
Chris Grabosky <chris@grabosky.net>
2021-02-05 10:59:21 -0500

    error in nav bar

---
+++ ServerPrep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/mdg-2018/cloud-manager-quick-launch.git
+
+apt update
+
+apt install -y docker.io
+
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
