39d80e3193b9070287e83c1d6bc243e8a89f702d
Chris Grabosky <chris@grabosky.net>
2022-04-26 10:53:50 -0400

    optional variable for an auth check
    returns {} if bad user, any json is good user

--- SPAWNHUB/SpawnHub/SpawnHub/__init__.py
+++ SPAWNHUB/SpawnHub/SpawnHub/__init__.py
@@ -27,117 +27,139 @@ class Handler_API_HelloWorld(tornado.web.RequestHandler):
 
 class Handler_CM_API_Launch(tornado.web.RequestHandler):
     async def get(self, username):
-        password = ""
-        count = "3"
-        expire = ""
-        node_agentVersion= ""
-        node_key_name= ""
-        node_instance_type= ""
-        om_ansible_python_interpreter= ""
-        om_key_name= ""
-        ansible_ssh_private_key_file= ""
-        ansible_python_interpreter=""
-        node_rootURL= ""
-        node_mmsGroupId= ""
-        node_mmsApiKey= ""
-        disk_size= ""
+        doRun = False
+        # if there is an auth checker, run it
+        if "AUTHCHECKURL" in os.environ:
+            resp = requests.get(os.environ['AUTHCHECKURL'])
+            if resp.json():
+                # everything checks out via auth, json was returned... 
+                doRun = True
+        else:
+            doRun = True
+
+        if doRun:
+            password = ""
+            count = "3"
+            expire = ""
+            node_agentVersion= ""
+            node_key_name= ""
+            node_instance_type= ""
+            om_ansible_python_interpreter= ""
+            om_key_name= ""
+            ansible_ssh_private_key_file= ""
+            ansible_python_interpreter=""
+            node_rootURL= ""
+            node_mmsGroupId= ""
+            node_mmsApiKey= ""
+            disk_size= ""
 
 
-        if self.get_argument("password") != None:
-            password = self.get_argument("password")
-        if self.get_argument("count") != None:
-            count = self.get_argument("count")
-        if self.get_argument("node_agentVersion") != None:
-            node_agentVersion = self.get_argument("node_agentVersion")
-        if self.get_argument("node_key_name") != None:
-            node_key_name = self.get_argument("node_key_name")
-        if self.get_argument("node_instance_type") != None:
-            node_instance_type = self.get_argument("node_instance_type")
-        if self.get_argument("ansible_ssh_private_key_file") != None:
-            ansible_ssh_private_key_file = self.get_argument("ansible_ssh_private_key_file")
-        if self.get_argument("ansible_python_interpreter") != None:
-            ansible_python_interpreter = self.get_argument("ansible_python_interpreter")
-        if self.get_argument("node_rootURL") != None:
-            node_rootURL = self.get_argument("node_rootURL")
-        if self.get_argument("node_mmsGroupId") != None:
-            node_mmsGroupId = self.get_argument("node_mmsGroupId")
-        if self.get_argument("node_mmsApiKey") != None:
-            node_mmsApiKey = self.get_argument("node_mmsApiKey")
-        if self.get_argument("disk_size") != None:
-            disk_size = self.get_argument("disk_size")
-        if self.get_argument("expire") != None:
-            expire = self.get_argument("expire")
+            if self.get_argument("password") != None:
+                password = self.get_argument("password")
+            if self.get_argument("count") != None:
+                count = self.get_argument("count")
+            if self.get_argument("node_agentVersion") != None:
+                node_agentVersion = self.get_argument("node_agentVersion")
+            if self.get_argument("node_key_name") != None:
+                node_key_name = self.get_argument("node_key_name")
+            if self.get_argument("node_instance_type") != None:
+                node_instance_type = self.get_argument("node_instance_type")
+            if self.get_argument("ansible_ssh_private_key_file") != None:
+                ansible_ssh_private_key_file = self.get_argument("ansible_ssh_private_key_file")
+            if self.get_argument("ansible_python_interpreter") != None:
+                ansible_python_interpreter = self.get_argument("ansible_python_interpreter")
+            if self.get_argument("node_rootURL") != None:
+                node_rootURL = self.get_argument("node_rootURL")
+            if self.get_argument("node_mmsGroupId") != None:
+                node_mmsGroupId = self.get_argument("node_mmsGroupId")
+            if self.get_argument("node_mmsApiKey") != None:
+                node_mmsApiKey = self.get_argument("node_mmsApiKey")
+            if self.get_argument("disk_size") != None:
+                disk_size = self.get_argument("disk_size")
+            if self.get_argument("expire") != None:
+                expire = self.get_argument("expire")
 
-        if((node_mmsGroupId == "") and (node_mmsApiKey == "")):
-            with open('/opt/AnsibleContent/deployConfig.json', 'r') as myfile:
-                deployConfigContents=myfile.read()
-                deployConfig = json.loads(deployConfigContents)
-                node_mmsGroupId = deployConfig["node_mmsGroupId"]
-                node_mmsApiKey = deployConfig["node_mmsApiKey"]
-            
-        if(password == os.environ['APIPW']):
-            pbPath = "/opt/AnsibleContent/playbook.yaml"
-            uid = str(uuid.uuid4())[:8]
-            # build command to run an run it
-            # mad security
-            cmd = "ansible-playbook {0} --extra-vars 'ownerUserName={1} node_replSetCount={2} node_expireon={3} node_agentVersion={4} node_key_name={5} node_instance_type={6} ansible_ssh_private_key_file={7} ansible_python_interpreter={8} node_rootURL={9} node_mmsGroupId={10} node_mmsApiKey={11} node_disk_size={12}' > /opt/wwwroot/static/output/{1}_cm.txt 2>&1".format(pbPath,username,count, expire, node_agentVersion,node_key_name,node_instance_type,ansible_ssh_private_key_file,ansible_python_interpreter,node_rootURL,node_mmsGroupId,node_mmsApiKey,disk_size)
-            subprocess.Popen(cmd, shell=True, cwd="/opt/AnsibleContent",stdout = subprocess.PIPE)
-            self.write("Running, "+ username)
+            if((node_mmsGroupId == "") and (node_mmsApiKey == "")):
+                with open('/opt/AnsibleContent/deployConfig.json', 'r') as myfile:
+                    deployConfigContents=myfile.read()
+                    deployConfig = json.loads(deployConfigContents)
+                    node_mmsGroupId = deployConfig["node_mmsGroupId"]
+                    node_mmsApiKey = deployConfig["node_mmsApiKey"]
+                
+            if(password == os.environ['APIPW']):
+                pbPath = "/opt/AnsibleContent/playbook.yaml"
+                uid = str(uuid.uuid4())[:8]
+                # build command to run an run it
+                # mad security
+                cmd = "ansible-playbook {0} --extra-vars 'ownerUserName={1} node_replSetCount={2} node_expireon={3} node_agentVersion={4} node_key_name={5} node_instance_type={6} ansible_ssh_private_key_file={7} ansible_python_interpreter={8} node_rootURL={9} node_mmsGroupId={10} node_mmsApiKey={11} node_disk_size={12}' > /opt/wwwroot/static/output/{1}_cm.txt 2>&1".format(pbPath,username,count, expire, node_agentVersion,node_key_name,node_instance_type,ansible_ssh_private_key_file,ansible_python_interpreter,node_rootURL,node_mmsGroupId,node_mmsApiKey,disk_size)
+                subprocess.Popen(cmd, shell=True, cwd="/opt/AnsibleContent",stdout = subprocess.PIPE)
+                self.write("Running, "+ username)
 
 class Handler_OM_API_Launch(tornado.web.RequestHandler):
     async def get(self, username):
-        password = ""
-        expire = "" 
-        node_agentVersion= ""
-        node_key_name= ""
-        node_instance_type= ""
-        om_ansible_python_interpreter= ""
-        om_key_name= ""
-        ansible_ssh_private_key_file= ""
-        ansible_python_interpreter=""
-        node_rootURL= ""
-        node_mmsGroupId= ""
-        node_mmsApiKey= ""
-        disk_size= ""
+        doRun = False
+        # if there is an auth checker, run it
+        if "AUTHCHECKURL" in os.environ:
+            resp = requests.get(os.environ['AUTHCHECKURL'])
+            if resp.json():
+                # everything checks out via auth, json was returned... 
+                doRun = True
+        else:
+            doRun = True
+
+        if doRun:
+            password = ""
+            expire = "" 
+            node_agentVersion= ""
+            node_key_name= ""
+            node_instance_type= ""
+            om_ansible_python_interpreter= ""
+            om_key_name= ""
+            ansible_ssh_private_key_file= ""
+            ansible_python_interpreter=""
+            node_rootURL= ""
+            node_mmsGroupId= ""
+            node_mmsApiKey= ""
+            disk_size= ""
 
 
-        if self.get_argument("password") != None:
-            password = self.get_argument("password")
-        if self.get_argument("node_agentVersion") != None:
-            node_agentVersion = self.get_argument("node_agentVersion")
-        if self.get_argument("node_key_name") != None:
-            node_key_name = self.get_argument("node_key_name")
-        if self.get_argument("node_instance_type") != None:
-            node_instance_type = self.get_argument("node_instance_type")
-        if self.get_argument("ansible_ssh_private_key_file") != None:
-            ansible_ssh_private_key_file = self.get_argument("ansible_ssh_private_key_file")
-        if self.get_argument("ansible_python_interpreter") != None:
-            ansible_python_interpreter = self.get_argument("ansible_python_interpreter")
-        if self.get_argument("node_rootURL") != None:
-            node_rootURL = self.get_argument("node_rootURL")
-        if self.get_argument("node_mmsGroupId") != None:
-            node_mmsGroupId = self.get_argument("node_mmsGroupId")
-        if self.get_argument("node_mmsApiKey") != None:
-            node_mmsApiKey = self.get_argument("node_mmsApiKey")
-        if self.get_argument("disk_size") != None:
-            disk_size = self.get_argument("disk_size")
-        if self.get_argument("password") != None:
-            password = self.get_argument("password")
-        if self.get_argument("expire") != None:
-            expire = self.get_argument("expire")
-        if self.get_argument("om_key_name") != None:
-            om_key_name = self.get_argument("om_key_name")
-        if self.get_argument("om_ansible_python_interpreter") != None:
-            om_ansible_python_interpreter = self.get_argument("om_ansible_python_interpreter")
-            
-        if(password == os.environ['APIPW']):
-            pbPath = "/opt/AnsibleContent/omPlaybook.yaml"
-            uid = str(uuid.uuid4())[:8]
-            # build command to run an run it
-            cmd = "ansible-playbook {0} --extra-vars 'ownerUserName={1} node_replSetCount={2} om_expireon={3} node_agentVersion={4} node_key_name={5} node_instance_type={6} ansible_ssh_private_key_file={7} ansible_python_interpreter={8} node_rootURL={9} node_mmsGroupId={10} node_mmsApiKey={11} om_disk_size={12} om_key_name={13} om_ansible_python_interpreter={14}' > /opt/wwwroot/static/output/{1}_om.txt 2>&1".format(pbPath,username,"", expire, node_agentVersion,node_key_name,node_instance_type,ansible_ssh_private_key_file,ansible_python_interpreter,node_rootURL,node_mmsGroupId,node_mmsApiKey,disk_size,om_key_name,om_ansible_python_interpreter)
+            if self.get_argument("password") != None:
+                password = self.get_argument("password")
+            if self.get_argument("node_agentVersion") != None:
+                node_agentVersion = self.get_argument("node_agentVersion")
+            if self.get_argument("node_key_name") != None:
+                node_key_name = self.get_argument("node_key_name")
+            if self.get_argument("node_instance_type") != None:
+                node_instance_type = self.get_argument("node_instance_type")
+            if self.get_argument("ansible_ssh_private_key_file") != None:
+                ansible_ssh_private_key_file = self.get_argument("ansible_ssh_private_key_file")
+            if self.get_argument("ansible_python_interpreter") != None:
+                ansible_python_interpreter = self.get_argument("ansible_python_interpreter")
+            if self.get_argument("node_rootURL") != None:
+                node_rootURL = self.get_argument("node_rootURL")
+            if self.get_argument("node_mmsGroupId") != None:
+                node_mmsGroupId = self.get_argument("node_mmsGroupId")
+            if self.get_argument("node_mmsApiKey") != None:
+                node_mmsApiKey = self.get_argument("node_mmsApiKey")
+            if self.get_argument("disk_size") != None:
+                disk_size = self.get_argument("disk_size")
+            if self.get_argument("password") != None:
+                password = self.get_argument("password")
+            if self.get_argument("expire") != None:
+                expire = self.get_argument("expire")
+            if self.get_argument("om_key_name") != None:
+                om_key_name = self.get_argument("om_key_name")
+            if self.get_argument("om_ansible_python_interpreter") != None:
+                om_ansible_python_interpreter = self.get_argument("om_ansible_python_interpreter")
+                
+            if(password == os.environ['APIPW']):
+                pbPath = "/opt/AnsibleContent/omPlaybook.yaml"
+                uid = str(uuid.uuid4())[:8]
+                # build command to run an run it
+                cmd = "ansible-playbook {0} --extra-vars 'ownerUserName={1} node_replSetCount={2} om_expireon={3} node_agentVersion={4} node_key_name={5} node_instance_type={6} ansible_ssh_private_key_file={7} ansible_python_interpreter={8} node_rootURL={9} node_mmsGroupId={10} node_mmsApiKey={11} om_disk_size={12} om_key_name={13} om_ansible_python_interpreter={14}' > /opt/wwwroot/static/output/{1}_om.txt 2>&1".format(pbPath,username,"", expire, node_agentVersion,node_key_name,node_instance_type,ansible_ssh_private_key_file,ansible_python_interpreter,node_rootURL,node_mmsGroupId,node_mmsApiKey,disk_size,om_key_name,om_ansible_python_interpreter)
 
-            subprocess.Popen(cmd, shell=True, cwd="/opt/AnsibleContent",stdout = subprocess.PIPE)
-            self.write("Running, "+ username)
+                subprocess.Popen(cmd, shell=True, cwd="/opt/AnsibleContent",stdout = subprocess.PIPE)
+                self.write("Running, "+ username)
 
 class Handler_API_List(tornado.web.RequestHandler):
     def set_default_headers(self):
